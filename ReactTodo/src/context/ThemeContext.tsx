31efb5a6a23a4b16a208ac7bea9e60db845b2924
MohammadKaalam <m.kaalam4367@gmail.com>
2025-08-06 01:11:13 +0330

    Add ThemeContext to manage dark mode globally

---
+++ ReactTodo/src/context/ThemeContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useEffect, useState, type ReactNode } from "react";
+
+interface ThemeContextType {
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+  darkMode: false,
+  toggleDarkMode: () => {},
+});
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('darkMode');
+    if (savedTheme) {
+      setDarkMode(savedTheme === 'true');
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('bg-dark', 'text-light');
+    } else {
+      document.body.classList.remove('bg-dark', 'text-light');
+    }
+    localStorage.setItem('darkMode', darkMode.toString());
+  }, [darkMode]);
+
+  const toggleDarkMode = () => setDarkMode((prev) => !prev);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
