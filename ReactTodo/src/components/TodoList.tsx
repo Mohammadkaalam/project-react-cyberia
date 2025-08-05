9aea96f89893c1c00b98c66cc1ca8bfe4a483964
MohammadKaalam <m.kaalam4367@gmail.com>
2025-08-06 01:17:46 +0330

    edit

--- ReactTodo/src/components/TodoList.tsx
+++ ReactTodo/src/components/TodoList.tsx
@@ -1,6 +1,6 @@
 import { useAppSelector } from '../store/hooks';
 import TodoItem from './TodoItem';
-import { useState } from 'react';
+import { useState } from 'react'; 
 
 export default function TodoList() {
   const todos = useAppSelector((state) => state.todos.items);
--- ReactTodo/src/pages/Home.tsx
+++ ReactTodo/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import TodoList from '../components/TodoList';
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="d-flex justify-content-between align-items-center mb-3">
         <DarkModeToggle />
       </div>
       <TodoForm />
