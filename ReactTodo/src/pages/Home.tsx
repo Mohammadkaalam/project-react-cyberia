663dda47c2a328f67156699b662a5ac68143275e
MohammadKaalam <m.kaalam4367@gmail.com>
2025-08-06 01:14:18 +0330

    Use DarkModeToggle in Home component

--- ReactTodo/src/pages/Home.tsx
+++ ReactTodo/src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import DarkModeToggle from '../components/DarkModeToggle';
 import TodoForm from '../components/TodoForm';
 import TodoList from '../components/TodoList';
 
 export default function Home() {
   return (
     <>
+      <div>
+        <DarkModeToggle />
+      </div>
       <TodoForm />
       <TodoList />
     </>
